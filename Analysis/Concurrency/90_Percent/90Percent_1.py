271e82e1b11671df216af6ccf1a32aa3682a91e8
JayathmaChathurangani <ejcjayathma@gmail.com>
2018-10-02 08:13:08 +0530

    adding latency plots-python

---
+++ Analysis/Concurrency/90_Percent/90Percent_1.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import csv
+import sys
+
+dataset_1 = sys.argv[1]
+plot_title= sys.argv[2]
+
+x1=[]
+y1=[]
+
+with open(dataset_1, 'r') as csvfile:
+    plots= csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        x1.append(int(row[0]))
+        y1.append(float(row[4]))
+
+plt.plot(x1,y1, marker='o')
+plt.title(plot_title)
+plt.xlabel('Concurrency')
+plt.ylabel('90 Percentile')
+
+plt.show()
