91f6c403196ee7a76477bb6b14d19e91f6f57ded
gayalkuruppu <gayalkuruppu.gk@gmail.com>
2019-12-03 12:05:45 +0530

    Added python files for plotting results

---
+++ Analysis/Plotting/4_DB_Cache_Orchestration.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.rcParams.update({'font.size': 14})
+
+path = '/home/gayal/PycharmProjects/microservices-performance/'
+csv = 'Results/4_DB_Cache_Orchestration.csv'
+save_to = 'Analysis/Plotting/plots/4_DB_Cache_Orchestration/'
+data = pd.read_csv(path+csv)
+
+
+for s in range(0, 1, 24):
+    plt.figure()
+    # plt.title('DB cache - Chaining pattern')
+    plt.xlabel('Concurrency')
+    plt.ylabel('Throughput (transactions/second)')
+    plt.plot(data['Users'].iloc[s:s+8], data['Throughput'].iloc[s:s+8], label='One Service', marker='o')
+    plt.plot(data['Users'].iloc[s+8:s+16], data['Throughput'].iloc[s+8:s+16], label='Two Services', marker='o')
+    # plt.plot(data['Users'].iloc[s+16:s+24], data['Throughput'].iloc[s+16:s+24], label='Three Services', marker='o')
+    #
+    # this is used only in chaining pattern where we have data for 3 services case. otherwise leave this line commented
+    #
+    plt.ticklabel_format(style='sci', axis='y', scilimits=(3, 3))
+    plt.legend()
+    plt.savefig(path+save_to+'/TPS_plot'+str(s//24), bbox_inches='tight')
+
+for s in range(0, 1, 24):
+    plt.figure()
+    # plt.title('DB cache - Chaining pattern')
+    plt.xlabel('Concurrency')
+    plt.ylabel('Average Latency (ms)')
+    plt.plot(data['Users'].iloc[s:s+8], data['Avg_Latency'].iloc[s:s+8], label='One Service', marker='o')
+    plt.plot(data['Users'].iloc[s+8:s+16], data['Avg_Latency'].iloc[s+8:s+16], label='Two Services', marker='o')
+    # plt.plot(data['Users'].iloc[s+16:s+24], data['Avg_Latency'].iloc[s+16:s+24], label='Three Services', marker='o')
+    plt.legend()
+    plt.savefig(path+save_to+'/Latency_plot'+str(s//24), bbox_inches='tight')
